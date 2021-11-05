f243e86be1467a091c23839888b58b231988dbac
Visshal Suresh <skvisshal@gmail.com>
2021-11-05 14:59:35 -0400

    mid phase 03

--- visshal_alex_movie_buff_project/movie_buff/03_queries.rb
+++ visshal_alex_movie_buff_project/movie_buff/03_queries.rb
@@ -1,24 +1,28 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
-
+  Movie.select(:title, :id).joins(:actors).where(actors: {name: those_actors})
+  .group(:id).having('COUNT(actors.id) = ?', those_actors.length)
 end
 
 def golden_age
   # Find the decade with the highest average movie score.
-
+  Movie.select('AVG(score) AS average, (yr / 10) * 10 AS decade').group('decade')
+  .order('AVG(score) DESC').first.decade
 end
 
 def costars(name)
   # List the names of the actors that the named actor has ever
   # appeared with.
   # Hint: use a subquery
-
+  sq = Movie.select(:id).joins(:actors).where(actors: {name: name})
+  Movie.joins(:actors).where(movies: {id: sq}).where.not(actors: {name: name})
+  .distinct.pluck(:name)
 end
 
 def actor_out_of_work
   # Find the number of actors in the database who have not appeared in a movie
-
+  
 end
 
 def starring(whazzername)
